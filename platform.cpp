296b82d425c6637488344cc3ff225d3434d8a578
h-tanaka <haruki_tanaka@outlook.jp>
2025-10-24 10:28:59 +0900

    debug7

--- platform.cpp
+++ platform.cpp
@@ -35,7 +35,7 @@ DigitalOut  PWR_EN_PIN(PLATFORM_PWR_EN_PIN);
 void init_IO()
 {
     // Initialize I2C bus (400 kHz)
-    i2c.frequency(400000);
+    i2c.frequency(4);
 
     // Replicate vendor sample reset_device() sequence:
     // 1. Pulse PWR_EN to power on sensor
